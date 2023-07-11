d0fd96a1434007d53d3b0698755157b252cce826
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-11 13:48:14 +0530

    added bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts=[
+    {
+        'title':'Blog Post 1',
+        'author':'CoreyMS',
+        'content':'First post content',
+        'date_posted':'July 11, 2023'
+    },
+    {
+        'title':'Blog Post 2',
+        'author':'Priyanshu Naskar',
+        'content':'Seconf post content',
+        'date_posted':'July 11, 2023'
+    }
+]
+
+def home(request):
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context=context)
+
+def about(request):
+    return render(request,'blog/about.html',context={'title':'About'})
