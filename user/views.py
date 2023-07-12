cff0287ee3901608138ef4607fba09c40fcb056b
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-12 12:54:13 +0530

    user authorizatrion done

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.forms import ValidationError
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserRegisterForm
 
@@ -10,9 +11,13 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             username=form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created please login')
             form.save()
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request,'user/profile.html')
