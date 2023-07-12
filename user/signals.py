0510f750f4fe2ffecb578ccdf7ede2d17c438d0f
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-12 18:03:26 +0530

    files getting deleted on change of profile picture

--- user/signals.py
+++ user/signals.py
@@ -1,7 +1,7 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,pre_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-
+import os 
 
 from .models import Profile
 
@@ -14,3 +14,20 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save,sender=User)
 def save_profile(sender,instance,**kwargs):
     instance.profile.save()
+
+@receiver(pre_save, sender=Profile)
+def delete_old_file(sender, instance, **kwargs):
+    # on creation, signal callback won't be triggered 
+    if instance._state.adding and not instance.pk:
+        return False
+    
+    try:
+        old_file = sender.objects.get(pk=instance.pk).image
+    except sender.DoesNotExist:
+        return False
+    
+    # comparing the new file with the old one
+    file = instance.image
+    if not old_file == file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
