b36cec57c2f8689fdc3c9427125efc868bde49cc
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-13 17:23:10 +0530

    added detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('',views.home,name='blog-home'),
-    path('about/',views.about,name='blog-about'),
+urlpatterns = [
+    # path('',views.home,name='blog-home'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,28 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from . models import Post
 
+
 def home(request):
-    posts=Post.objects.all()
-    context={
-        'posts':posts
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
     }
-    return render(request,'blog/home.html',context=context)
+    return render(request, 'blog/home.html', context=context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # newest to oldest
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
-    return render(request,'blog/about.html',context={'title':'About'})
-    
+    return render(request, 'blog/about.html', context={'title': 'About'})
