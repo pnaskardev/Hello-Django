fa970862a50cd8a62d990f5941ad935f5073b7a2
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-14 13:56:28 +0530

    create read update delete is done

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
-from typing import Any
+from typing import Any, Optional
 from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from . models import Post
 
@@ -20,9 +29,44 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']  # newest to oldest
 
+
 class PostDetailView(DetailView):
     model = Post
-    
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url='/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request, 'blog/about.html', context={'title': 'About'})
